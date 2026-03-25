1e278f8055cb2a89749eeae13f87473a037d0075
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-03-25 15:05:20 +0900

    Initialize project with Makepad added

--- src/components/mod.rs
+++
@@ -1,1 +0,0 @@
-pub(crate) mod icon_button;
--- src/metadata/track_meta.rs
+++
@@ -1,8 +0,0 @@
-use eframe::egui;
-
-pub struct TrackMeta {
-    pub name: String,
-    pub color: egui::Color32,
-    pub is_muted: bool,
-    pub is_solo: bool,
-}
